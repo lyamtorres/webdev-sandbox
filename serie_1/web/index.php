1f25745320b8bc3b1f02967cbf9cf9ac3185bc8b
lyamtorres <lyamtorres@hotmail.com>
2022-01-23 20:26:53 +0100

    Question 4 terminée

--- serie_1/web/index.php
+++ serie_1/web/index.php
@@ -3,8 +3,8 @@
 require_once __DIR__.'/../app/Controllers/ContactController.php';
 require_once __DIR__.'/../app/Models/ContactModel.php';
 require_once __DIR__.'/../util/View.php';
+require_once __DIR__.'/../util/Route.php';
 
-use Controllers\ContactController;
+use Util\Route;
 
-$listContacts = new ContactController();
-$listContacts->listContacts();
+$route = new Route();
